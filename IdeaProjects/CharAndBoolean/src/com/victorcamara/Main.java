e15170a809d5f1761640aed13d15c5bf0bbca264
Victor Camara Damasceno <Victor Camara Damasceno>
2018-05-28 15:52:00 -0300

    Finishes CharAndBoolean project

--- IdeaProjects/CharAndBoolean/src/com/victorcamara/Main.java
+++ IdeaProjects/CharAndBoolean/src/com/victorcamara/Main.java
@@ -3,6 +3,14 @@ package com.victorcamara;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //width of 16 (2 bytes)
+        char myChar = '\u03A9';
+        System.out.println("Unicode output was: " + myChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+        char registered = '\u00AE';
+        System.out.println("Registered symbol = " + registered);
     }
 }
