7a027d1e4deae4c971cb8ddc3d8ad5dc9c3951f6
Victor Camara Damasceno <Victor Camara Damasceno>
2018-05-28 15:59:09 -0300

    Create Strings project

---
+++ IdeaProjects/Strings/src/com/victorcamara/Main.java
@@ -0,0 +1,15 @@
+package com.victorcamara;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// byte
+    // short
+    // int
+    // long
+    // float
+    // double
+    // char
+    // boolean
+    }
+}
