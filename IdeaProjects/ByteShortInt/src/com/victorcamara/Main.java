9c4c4a9f79c69245de0ba22abbbdfc3a468a4661
Victor Camara Damasceno <Victor Camara Damasceno>
2018-05-28 10:05:44 -0300

    Show data types for numbers

--- IdeaProjects/ByteShortInt/src/com/victorcamara/Main.java
+++ IdeaProjects/ByteShortInt/src/com/victorcamara/Main.java
@@ -3,6 +3,24 @@ package com.victorcamara;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //int has a width of 32
+        int myMinValue = -2_147_483_648;
+        int myMaxValue = 2_147_483_647;
+        int myTotal = (myMinValue/2);
+        System.out.println("myTotal = " + myTotal);
+
+        //byte has a width of 8
+        byte myByteValue = -128;
+        byte myNewByteValue = (byte)(myByteValue/2);
+        System.out.println("myNewByteValue = " + myNewByteValue);
+
+        //short has a width of 16
+        short myShortValue = 32_762;
+        short myNewShortValue = (short)(myShortValue/2);
+        System.out.println("myNewShortValue = " + myNewShortValue);
+
+        //long has  a width of 64
+        long myLongValue = 9_223_372_036_854_775_807L;
     }
 }
