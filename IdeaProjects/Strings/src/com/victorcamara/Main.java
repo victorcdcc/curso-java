9ee9d5716c370d16ffcf8cf3b2771c046609b457
Victor Camara Damasceno <Victor Camara Damasceno>
2018-05-28 16:15:20 -0300

    Finishes String project

--- IdeaProjects/Strings/src/com/victorcamara/Main.java
+++ IdeaProjects/Strings/src/com/victorcamara/Main.java
@@ -3,13 +3,33 @@ package com.victorcamara;
 public class Main {
 
     public static void main(String[] args) {
-	// byte
-    // short
-    // int
-    // long
-    // float
-    // double
-    // char
-    // boolean
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " +  myString);
+        myString = myString + " \u00A9 2018";
+        System.out.println("myString is equal to " + myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println("The result is " + numberString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("lastString is equal to " + lastString);
+
+        double doubleNumber = 120.47;
+        lastString = lastString + doubleNumber;
+        System.out.println("lastString value: " + lastString);
     }
 }
