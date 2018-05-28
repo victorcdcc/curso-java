0657bec267415a349d789a029480d49f892d465d
Victor Camara Damasceno <Victor Camara Damasceno>
2018-05-28 11:17:18 -0300

    Primitive Number Types exercise

--- IdeaProjects/ByteShortInt/src/com/victorcamara/Main.java
+++ IdeaProjects/ByteShortInt/src/com/victorcamara/Main.java
@@ -3,6 +3,14 @@ package com.victorcamara;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        byte baite = 10;
+        short xorte = 20;
+        int inti = 50;
+        long longue = 50000L + 10L * (baite + xorte + inti);
+
+        short shorte = (short)(1000 + 10 * (baite + xorte + inti));
+
+        System.out.println("longue = " + longue);
+        System.out.println("shorte = " + shorte);
     }
 }
