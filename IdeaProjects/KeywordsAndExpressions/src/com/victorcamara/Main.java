9b3b3ef428e3fc9da819bb9361d7cb8b946617e6
Victor Camara Damasceno <Victor Camara Damasceno>
2018-05-29 08:56:29 -0300

    Finishes KeywordsAndExpressions and Creates StatementWhiteSpacesAndIndenting project

--- IdeaProjects/KeywordsAndExpressions/src/com/victorcamara/Main.java
+++ IdeaProjects/KeywordsAndExpressions/src/com/victorcamara/Main.java
@@ -3,6 +3,19 @@ package com.victorcamara;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50){
+            System.out.println("this is a express");
+        }
+
+        //Everything that is inside brackets are expressions.
+        //The variable that receives any type of data when it's initialized fits
+        //as an expression too.
+        //It's important too say that the semicolon, the "if" statement and the
+        //type of the variable are not part of the expression
+
     }
 }
