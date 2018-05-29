666ca51bc0f9b5835c8523398ff1e9114f41e2c2
Victor Camara Damasceno <Victor Camara Damasceno>
2018-05-29 09:21:32 -0300

    Finishes WhiteSpaceAndIndenting

--- IdeaProjects/StatementWhiteSpaceAndIndenting/src/com/victorcamara/Main.java
+++ IdeaProjects/StatementWhiteSpaceAndIndenting/src/com/victorcamara/Main.java
@@ -1,8 +1,21 @@
 package com.victorcamara;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        int myVariable = 50;
+
+        if (myVariable == 50){
+            System.out.println("Printed");
+        }
+
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is a test");
+        System.out.println("This is" +
+                "another" +
+                "still more.");
+
+        int anotherVariable = 50;myVariable--;System.out.println("This is another one");
     }
 }
