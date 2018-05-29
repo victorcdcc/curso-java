e57a07ff4b50f5fcb2528e71d56bd2ac120f0292
Victor Camara Damasceno <Victor Camara Damasceno>
2018-05-29 11:27:23 -0300

    Fixes second bug and shows how to solve the same problem with a diferent code. Also finishes the Method project

--- IdeaProjects/Methods/src/com/victorcamara/Main.java
+++ IdeaProjects/Methods/src/com/victorcamara/Main.java
@@ -28,7 +28,6 @@ public class Main {
         displayHighScorePosition("Ribeiro", calculateHighScorePosition(100));
 
 
-
     }
 
 /*    public static void main(String[] args) {
@@ -55,15 +54,25 @@ public class Main {
                 + playerPosition + "º position on the high score table.");
     }
 
-    public static int calculateHighScorePosition(int playerScore){
-        if(playerScore >= 1000){
-            return 1;
-        } else if (playerScore >= 500 && playerScore < 1000){
-            return 2;
-        } else if (playerScore >= 100 && playerScore < 500){
-            return 3;
-        } else {
-            return 4;
+    public static int calculateHighScorePosition(int playerScore) {
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500) {
+//            return 2;
+//        } else if (playerScore >= 100) {
+//            return 3;
+//        }
+//        return 4;
+        int position = 4; //assuming position 4 will be returned
+
+        if (playerScore >= 1000) {
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100) {
+            position = 3;
         }
+
+        return position;
     }
 }
