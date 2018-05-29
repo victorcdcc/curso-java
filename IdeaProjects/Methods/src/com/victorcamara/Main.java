f750437de189b54c534c6595bfab95e0a469be7f
Victor Camara Damasceno <Victor Camara Damasceno>
2018-05-29 11:18:13 -0300

    Creates the challenge and fixes the first bug

--- IdeaProjects/Methods/src/com/victorcamara/Main.java
+++ IdeaProjects/Methods/src/com/victorcamara/Main.java
@@ -9,13 +9,25 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+
+        displayHighScorePosition("Nobrega", calculateHighScorePosition(1500));
+        displayHighScorePosition("Camara", calculateHighScorePosition(900));
+        displayHighScorePosition("Gouveia", calculateHighScorePosition(400));
+        displayHighScorePosition("Monteiro", calculateHighScorePosition(50));
+        displayHighScorePosition("Benigno", calculateHighScorePosition(1000));
+        displayHighScorePosition("Meira", calculateHighScorePosition(500));
+        displayHighScorePosition("Ribeiro", calculateHighScorePosition(100));
+
+
 
     }
 
@@ -32,10 +44,26 @@ public class Main {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score was " + finalScore);
             return finalScore;
         }
-        
+
         return -1;
+    }
+
+    public static void displayHighScorePosition(String playerName, int playerPosition) {
+        System.out.println(playerName + " managed to get into "
+                + playerPosition + "º position on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+        if(playerScore >= 1000){
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000){
+            return 2;
+        } else if (playerScore >= 100 && playerScore < 500){
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }
