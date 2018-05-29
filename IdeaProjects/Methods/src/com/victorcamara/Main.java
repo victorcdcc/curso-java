49403c8738fd42efe53ad5572b6cb69e7ed9b025
Victor Camara Damasceno <Victor Camara Damasceno>
2018-05-29 10:45:51 -0300

    Finishes the first part of Methods project

--- IdeaProjects/Methods/src/com/victorcamara/Main.java
+++ IdeaProjects/Methods/src/com/victorcamara/Main.java
@@ -3,6 +3,39 @@ package com.victorcamara;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+    }
+
+/*    public static void main(String[] args) {
+
+        calculateScore(true, 800, 5, 100);
+
+        calculateScore(true, 10000, 8, 200);
+
+    }*/
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            System.out.println("Your final score was " + finalScore);
+            return finalScore;
+        }
+        
+        return -1;
     }
 }
