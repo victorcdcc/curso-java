e99009d6a70ee03f0400c4a3f5f892b38e0ca1e7
Victor Camara Damasceno <Victor Camara Damasceno>
2018-06-15 10:24:13 -0300

    Finishes BarkingDog, add and finish LeapYearCalculator project

---
+++ IdeaProjects/LeapYearCalculator/src/com/victorcamara/Main.java
@@ -0,0 +1,18 @@
+package com.victorcamara;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(-1600));
+    }
+
+    public static boolean isLeapYear(int year){
+        if(!(year >= 1 && year <= 9999)){
+            return false;
+        } else if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
